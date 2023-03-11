fd655c12416d1f39ac6bc8b39e5df1dbaf1d7279
“[firstname <nirupamadissanayake23@gmail.com>
2023-03-11 22:30:49 +0530

    Add Bank Deposit Views

--- view/student/bankDeposit.php
+++ view/student/bankDeposit.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Checkout</title>
-    <link rel="stylesheet" href="../../public/css/checkout.css?<?php echo time(); ?>">
+    <title>Checkout-Bank Deposit</title>
+    <link rel="stylesheet" href="../../public/css/bankDeposit.css?php echo time(); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
@@ -38,7 +38,7 @@ $viewCartController = new viewCartController();
 
 <div id="checkout-container">
     <div id="checkout">
-        <b><p id="title"><a href="../../view/student/cart.php"><span id="cart-shortcut">Cart</span></a>&nbsp;&nbsp;>&nbsp;&nbsp;Checkout</p></b>
+        <b><p id="title"><a href="../../view/student/cart.php"><span id="cart-shortcut">Cart</span></a>&nbsp;&nbsp;>&nbsp;&nbsp;<a href="../../view/student/checkout.php"><span id="cart-shortcut">Checkout</span></a>&nbsp;&nbsp;>&nbsp;&nbsp;Bank Deposit Payment</p></b>
         <br><hr><br>
         <div id="checkout-contents">
             <div id="cart-container">
@@ -127,22 +127,50 @@ $viewCartController = new viewCartController();
             </div>
 
             <div id="option-container">
-                <p id="payment-title">Choose your payment method </p>
-                <a href="">
-                    <button id="card-payment-btn" class="payment-btns">
-                        Card Payment
-                    </button>
-                </a>
-                <a href="./bankDeposit.php">
-                    <button id="bank-deposit-btn" class="payment-btns">
-                        Bank Deposit
-                    </button>
-                </a>
-                <br>
-                <img src="../../public/img/payments.svg" id="payment-img">
+                <p id="payment-title">After the bank transfer to the below account, upload the payment slip to verify the payment.</p>
+                <b><p>Bank Details:</p></b>
+
+                <!-- get these details from the database -->
+                <pre>
+                
+    Bank : Commercial Bank PLC
+    Branch : Kandy
+    Account Holder's Name : Master.lk(Pvt) Ltd
+    Account Number : 200089768
+                </pre>
+                <!-- upto here -->
+
+                <div id="upload-slip-sec">
+                    <!-- <form action="" method="post" enctype="multipart/form-data" id="slip-upload-form"> -->
+                        <label for="images" class="drop-container">
+                            <span class="drop-title">Drop files here</span>
+                            or
+                            <input type="file" name="image" id="upload-photo-space" oninput="inputChange()">
+                        </label>
+                        <button type="submit" name="photo-upload-submit" id="photo-upload-submit" onclick="getUploadSuccessPopup()">Upload</button>
+                    </form>
+                <!-- </div> -->
 
             </div>
         </div>
 
     </div>
 </div>
+
+<div class="page-mask" id="page-mask-upload-success">
+    <div id="upload-success-popup">
+        <img id="success-icon" src="../../public/icons/success-yellow.svg">
+        <b><p id="upload-title">Uploaded Successfully!</p></b>
+        <button onclick="closeUploadSuccessPopup()" class="close-button">
+            <img src="../../public/icons/close.svg" class="close-icon">
+        </button>
+        <p id="upload-success-text">Slip has been successfully uploaded. Verification updates by the Administration will reach you soon.</p>
+        <button id="ok-btn" onclick="closeUploadSuccessPopup()">OK</button>
+        
+    </div>
+</div>
+
+<script src="../../public/js/bankDeposit.js"></script>
+
+</body>
+</html>
