068f35e6c291da46494b03ad0edb993283c2ea44
Charith Sathsara <charithsathsara169@gmail.com>
2023-03-14 19:30:11 +0530

    Commiting Authentication.php Changes

--- controller/authController/authentication/Authentication.php
+++ controller/authController/authentication/Authentication.php
@@ -5,7 +5,6 @@ class Authentication{
     public static function userAuthentication(){
         
         if(!isset($_SESSION['authenticated'])){
-            echo"awaaaaa";
             restrict_access_redirect("Please login to access the pages", 'view/authentication/index.php');
             return false;
         }else{
