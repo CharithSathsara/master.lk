9778a3edd457407b11ccbfd97240e0b25cdd5ba4
Nadun Viduranga <89892888+nadunvuduranga@users.noreply.github.com>
2023-05-09 18:02:44 +0530

    Adding Model Content Creator

---
+++ model/ContentCreator.php
@@ -0,0 +1,81 @@
+<?php
+
+class ContentCreator
+{
+    public static function updateContentCreator($userId,$fname,$lname,$address1,$address2,$number,$email,$subject,$connection){
+
+        $query = "UPDATE user 
+                  SET firstName='$fname',lastName='$lname',email= '$email' ,mobile= '$number' ,addLine01= '$address1' ,addLine02= '$address2'
+                  WHERE userId ='$userId'";
+
+        $result = $connection->query($query);
+
+        // return $result;
+
+        if($result){
+
+            $query1 = "SELECT subjectId FROM subject WHERE subjectTitle ='$subject'";
+            $result1 = $connection->query($query1);
+
+            $subjectID = $result1->fetch_assoc();
+            $subjectId = $subjectID['subjectId'];
+
+            $query2 = "UPDATE contentcreator
+                    SET subjectId ='$subjectId'
+                    WHERE creatorId ='$userId'";
+
+            $finalResult = $connection->query($query2);
+
+
+            return $finalResult;
+        }else{
+            return false;
+        }
+    }
+
+    public static function CheckContentId($contentId, $connection){
+        $query = "SELECT * FROM topic_content WHERE contentId = '$contentId'";
+        $id = $connection->query($query);
+        return $id;
+    }
+
+    public static function AddTheoryContents( $sectionNo, $selectTopic,$sectionContent, $visibility,  $contentCreatorId, $connection){
+
+
+        $select2 = "SELECT contentId FROM topic_content WHERE contentId = '$sectionNo'";
+
+
+        $result2 = mysqli_query($connection, $select2);
+
+        if(mysqli_num_rows($result2) > 0){
+            popup_redirect("Section No. Already Exists!","view/contentcreator/addTheory.php");
+        }
+        else{
+
+            if($visibility== "Visible"){
+                $visibility = 1;
+            }
+            elseif($visibility== "Not Visible"){
+                $visibility = 0;
+            }
+            $insert = "INSERT INTO topic_content (contentId, topicId , content, visibility, creatorId) VALUES ('$sectionNo','$selectTopic','$sectionContent','$visibility','$contentCreatorId')";
+
+            $data = $connection->query($insert);
+
+            return $data;
+
+        }
+    }
+    public static function deleteContentCreator($userId,$connection){
+        $query = "DELETE FROM user WHERE userId='$userId'";
+
+        $data = $connection->query($query);
+
+        if($data){
+            return $data;
+        }else{
+            return false;
+        }
+    }
+}
+?>
