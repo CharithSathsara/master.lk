3d2306631f2a586c400cc37958a31a4ce83f4a96
Charith Sathsara <charithsathsara169@gmail.com>
2023-05-09 19:07:42 +0530

    Changes 4

--- model/ContentCreator.php
+++ model/ContentCreator.php
@@ -1,9 +1,10 @@
 <?php
 
-<<<<<<< HEAD
     class ContentCreator
     {
-        public static function updateContentCreator($userId,$fname,$lname,$address1,$address2,$number,$email,$subject,$connection){
+
+        public static function updateContentCreator($userId, $fname, $lname, $address1, $address2, $number, $email, $subject, $connection)
+        {
 
             $query = "UPDATE user 
                   SET firstName='$fname',lastName='$lname',email= '$email' ,mobile= '$number' ,addLine01= '$address1' ,addLine02= '$address2'
@@ -13,7 +14,7 @@
 
             // return $result;
 
-            if($result){
+            if ($result) {
 
                 $query1 = "SELECT subjectId FROM subject WHERE subjectTitle ='$subject'";
                 $result1 = $connection->query($query1);
@@ -28,56 +29,23 @@
                 $finalResult = $connection->query($query2);
 
                 return $finalResult;
-            }else{
+            } else {
                 return false;
             }
         }
 
-        public static function deleteContentCreator($userId,$connection){
+        public static function deleteContentCreator($userId, $connection)
+        {
             $query = "DELETE FROM user WHERE userId='$userId'";
 
             $data = $connection->query($query);
 
-            if($data){
+            if ($data) {
                 return $data;
-            }else{
+            } else {
                 return false;
             }
         }
-    }
-=======
-class ContentCreator
-{
-    public static function updateContentCreator($userId,$fname,$lname,$address1,$address2,$number,$email,$subject,$connection){
-
-        $query = "UPDATE user 
-                  SET firstName='$fname',lastName='$lname',email= '$email' ,mobile= '$number' ,addLine01= '$address1' ,addLine02= '$address2'
-                  WHERE userId ='$userId'";
-
-        $result = $connection->query($query);
-
-        // return $result;
-
-        if($result){
-
-            $query1 = "SELECT subjectId FROM subject WHERE subjectTitle ='$subject'";
-            $result1 = $connection->query($query1);
-
-            $subjectID = $result1->fetch_assoc();
-            $subjectId = $subjectID['subjectId'];
-
-            $query2 = "UPDATE contentcreator
-                    SET subjectId ='$subjectId'
-                    WHERE creatorId ='$userId'";
-
-            $finalResult = $connection->query($query2);
-
-
-            return $finalResult;
-        }else{
-            return false;
-        }
-    }
 
     public static function CheckContentId($contentId, $connection){
         $query = "SELECT * FROM topic_content WHERE contentId = '$contentId'";
@@ -112,17 +80,6 @@ class ContentCreator
 
         }
     }
-    public static function deleteContentCreator($userId,$connection){
-        $query = "DELETE FROM user WHERE userId='$userId'";
-
-        $data = $connection->query($query);
-
-        if($data){
-            return $data;
-        }else{
-            return false;
-        }
-    }
 }
->>>>>>> 9778a3edd457407b11ccbfd97240e0b25cdd5ba4
+
 ?>
