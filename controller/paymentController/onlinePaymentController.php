d7f600800d3d74fb2ff29e683a5c0d4b3657a549
Charith Sathsara <charithsathsara169@gmail.com>
2023-05-12 01:29:42 +0530

    Add Mail to Payment

--- controller/paymentController/onlinePaymentController.php
+++ controller/paymentController/onlinePaymentController.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 include('../../config/app.php');
 include_once('../../model/Student.php');
 include_once('../../model/Cart.php');
@@ -77,6 +79,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['payment']) && $_POST[
             //clear the cart
             Student::clearCart($db_connection->getConnection(), $cartId );
 
+            //send email to student
+            $email = Student::getStudentEmail($db_connection->getConnection(), $_SESSION['auth_user']['userId']);
+
+            if($email) {
+
+                $body='<p>Dear user,</p>';
+                $body.='<p>We are writing to inform you that your payment for the course(s) you have selected has been successfully processed. Congratulations on taking the first step towards your educational journey!</p>';
+                $body.='<br><p>Your account has been updated and you can now access your courses by logging into your account. You will find the course materials in your dashboard.</p>';
+                $body.='<br><p>If you have any questions or concerns about the course, please feel free to contact us at master.lk@gmail.com. We will be happy to assist you in any way possible.</p>';
+                $body.='<br><p>Thank you for choosing Master.lk as your education partner. We wish you all the best for your future endeavors.</p>';
+                $body.='<br><p>Thanks,</p>';
+                $body.='<p>Master.lk Team</p>';
+
+                try {
+                    $mail = new PHPMailer();
+
+                    // Enable verbose debug output
+                    $mail->isSMTP();                        // Set mailer to use SMTP
+                    $mail->Host       = 'smtp.gmail.com;';    // Specify main SMTP server
+                    $mail->SMTPAuth   = true;               // Enable SMTP authentication
+                    $mail->Username   = 'cloudcharith@gmail.com';     // SMTP username
+                    $mail->Password   = 'kqlownfkdeeqvrob';         // SMTP password
+                    $mail->SMTPSecure = 'tls';              // Enable TLS encryption
+                    $mail->Port       = 587;
+
+                    $mail->setFrom('cloudcharith@gmail.com', 'Master.lk Academy');// Set sender of the mail
+
+                    $mail->addAddress($email);// Add a recipient
+
+                    $mail->isHTML(true);
+                    $mail->Subject = 'Payment Success - Access to Your Courses';
+                    $mail->Body    = $body;
+
+                    $mail->Send();
+
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error ";
+                }
+            }
         }
     }
 }
